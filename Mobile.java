2f72b3179c74f8b5d36edcc9cb98c04a8263a9aa
chandragupthan <30656995+chandragupthan@users.noreply.github.com>
2022-11-22 03:40:36 +0000

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Mobile {
+
+	public void makeCall()
+	{
+
+		System.out.println("Make a phone call");
+
+	}
+
+	public void sendMsg()
+	{
+
+		System.out.println("Send a text message");
+	}	
+
+	public static void main(String[] args) {
+
+		Mobile obj= new Mobile();
+		obj.makeCall();
+		obj.sendMsg();
+
+	}
+
+}
---
+++ TwoWheeler.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class TwoWheeler {
+
+
+	public static void main(String[] args) {
+		int noOfWheels = 2;
+		float mileage = 56.7f;
+		boolean isPunctured = false;
+		char bikeNameFirstLetter = 'B';
+
+		//TwoWheeler obj=new TwoWheeler();
+		System.out.println("noOfWheels is "+noOfWheels);
+		System.out.println("mileage is "+mileage);
+		System.out.println("isPunctured "+isPunctured);
+		System.out.println("bikeNameFirstLetter is "+bikeNameFirstLetter);
+
+
+	}
+
+
+}
